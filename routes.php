6643476c0f49d6e0c0855653c72e0370a1c5b93e
Pahliana <phliana007@gmail.com>
2023-05-19 16:58:47 +0800

    Membuat Halaman Tambah Data

--- routes.php
+++ routes.php
@@ -9,7 +9,10 @@ if (isset($_GET['page'])){
             case 'lokasiread':
                 file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include "pages/404.php";
             break;
-        default:
+            case 'lokasicreate':
+                file_exists('pages/admin/lokasicreate.php') ? include 'pages/admin/lokasicreate.php' : include "pages/404.php";
+            break;
+            default:
         include "pages/404.php";    
     }
 
