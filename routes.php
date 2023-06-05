4814e17fedfa08234ab5dc7805830bef167b4bf1
Pahliana <phliana007@gmail.com>
2023-06-05 20:54:04 +0800

    Membuat Halaman Tampil Data Karyawan

--- routes.php
+++ routes.php
@@ -44,6 +44,19 @@ if (isset($_GET['page'])){
             case 'bagiandelete':
                 file_exists('pages/admin/bagiandelete.php') ? include 'pages/admin/bagiandelete.php' : include "pages/404.php";
             break;
+
+            case 'karyawanread':
+                file_exists('pages/admin/karyawanread.php') ? include 'pages/admin/karyawanread.php' : include "pages/404.php";
+            break;
+            case 'karyawanbagian':
+                file_exists('pages/admin/karyawanbagian.php') ? include 'pages/admin/karyawanbagian.php' : include "pages/404.php";
+            break;
+            case 'bagianupdate':
+                file_exists('pages/admin/bagianupdate.php') ? include 'pages/admin/bagianupdate.php' : include "pages/404.php";
+            break;
+            case 'bagiandelete':
+                file_exists('pages/admin/bagiandelete.php') ? include 'pages/admin/bagiandelete.php' : include "pages/404.php";
+            break;
             default:
         include "pages/404.php";    
     }
