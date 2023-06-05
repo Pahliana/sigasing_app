0c0a947627a3b96f58f448401072d0c99988d18e
Pahliana <phliana007@gmail.com>
2023-06-05 17:24:36 +0800

    Perbaharuan Pesan

--- pages/admin/lokasidelete.php
+++ pages/admin/lokasidelete.php
@@ -1,18 +1,18 @@
-<?php 
-if(isset($_GET['id'])) { 
-    $id = $_GET['id']; 
- 
-    $database = new Database(); 
-    $db = $database->getConnection(); 
- 
-    $deleteSQL = "DELETE FROM lokasi WHERE id = ?"; 
-    $stmt = $db->prepare($deleteSQL); 
-    $stmt->bindParam(1, $_GET['id']); 
-    if ($stmt->execute()) { 
-        $_SESSION['hasil'] = true; 
-    } else { 
-        $_SESSION['hasil'] = false; 
-    } 
-} 
-    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>"; 
-?>
+<?php
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $database = new Database();
+    $connection = $database->getConnection();
+
+    $deleteSQL = "DELETE FROM lokasi WHERE id = ?";
+    $statement = $connection->prepare($deleteSQL);
+    $statement->bindParam(1, $id);
+    if ($statement->execute()) {
+        $_SESSION['hasil'] = true;
+        $_SESSION['pesan'] = 'Berhasil hapus data';
+    } else {
+        $_SESSION['hasil'] = false;
+        $_SESSION['pesan'] = 'Gagal hapus data';
+    }
+    echo '<meta http-equiv="refresh" content="0;url=?page=lokasiread">';
+}
